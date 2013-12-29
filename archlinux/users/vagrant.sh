47d3f6f0546cbdd6c7604237a6ef9a9d57f8666d
pandrew <letters@paulnotcom.se>
2013-12-29 13:52:30 +0100

    Test mr behaviour when var is removed

--- archlinux/users/vagrant.sh
+++ archlinux/users/vagrant.sh
@@ -4,5 +4,4 @@ USERNAME=vagrant
 GROUP="users"
 ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,sys,wireshark,vboxusers,kvm,libvirt"
 USERS_SHELL=bash
-MR_BOOTSTRAP=https://raw.github.com/pandrew/pandrew-home/master/.mrconfig
 
