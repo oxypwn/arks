258743067a3a9ac7a6022f867ca6609ea41cb3a3
pandrew <letters@paulnotcom.se>
2014-01-01 18:56:28 +0100

    Add docker group to user pandrew

--- archlinux/users/pandrew.sh
+++ archlinux/users/pandrew.sh
@@ -9,7 +9,7 @@ groupadd libvirt
 # load userspecific settings from here
 USERNAME=pandrew
 GROUP="users"
-ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,sys,wireshark,vboxusers,kvm,libvirt"
+ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,docker,sys,wireshark,vboxusers,kvm,libvirt"
 LOGIN_SHELL=/bin/zsh
 MR_BOOTSTRAP=https://raw.github.com/pandrew/pandrew-home/master/.mrconfig
 
