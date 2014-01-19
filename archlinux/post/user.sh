da0aeab1e765d1377685e2bcf077d7c7a378a825
pandrew <letters@paulnotcom.se>
2014-01-19 20:01:48 +0100

    Email on error
    This fixes a bug makeing keychainio not installing keys due to empty
    variable. It also fixes #2.

--- archlinux/post/user.sh
+++ archlinux/post/user.sh
@@ -17,13 +17,13 @@ for ((i=0; i<${#array[@]}; ++i)); do
             useradd -m -g "$GROUP" -G "$ADDTOGROUPS" -s "$LOGIN_SHELL" "$USERNAME"
             _mrbootstrap "$MR_BOOTSTRAP"
             if [[ ! -z $API_KEY_EMAIL && ! -z $EMAIL ]]; then
+                _keychainio
                 _injectRandomPassword
 
                 SUBJECT="${HOSTNAME} sent you Your password."
                 TEXT="$PASSWORD"
                 
                 _mailgun
-                _keychainio
             elif [[ ! -z $EMAIL ]]; then
                 _keychainio
                 _injectBasicPassword
@@ -31,6 +31,8 @@ for ((i=0; i<${#array[@]}; ++i)); do
                 _injectBasicPassword
             fi
         fi
+    unset EMAIL
+    unset MR_BOOTSTRAP
 done
 
 # Add root
