3f1a2f1f9028e224faf6a8ed6d893d1e1f69488a
pandrew <letters@paulnotcom.se>
2014-02-01 15:04:34 +0100

    Add vagrant email to support of keyimport.

--- archlinux/users/vagrant.sh
+++ archlinux/users/vagrant.sh
@@ -5,3 +5,4 @@ GROUP="users"
 ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,sys,wireshark,vboxusers,kvm,libvirt"
 # NOTICE! Using a shell not listed in /etc/shells with full path will dissallow login. Be shure to have correct path to shell.
 LOGIN_SHELL=/bin/bash
+EMAIL=vagrant.insecure.key@gmail.com
