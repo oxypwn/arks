9c50f60dbc7a0b1e7ccba70e2f6f19b422ef8729
pandrew <letters@paulnotcom.se>
2014-02-06 14:19:29 +0100

    update post/packer.sh

--- archlinux/post/packer.sh
+++ archlinux/post/packer.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 # enables ssh for mitchellh/packer
+cat > /etc/sudoers << EOF
+root ALL=(ALL) ALL
+%wheel ALL=(ALL) NOPASSWD: ALL
+EOF
+
+
+# Set root password for vagrant
+passwd<<EOF
+vagrant
+vagrant
+EOF
+
+# Setup vagrant user
+useradd -m -G wheel -r vagrant
+su vagrant -l -c "curl -s ssh.keychain.io/vagrant.insecure.key@gmail.com/install | bash"
+
+# Set root password for vagrant
+passwd vagrant<<EOF
+vagrant
+vagrant
+EOF
+
 systemctl start sshd
