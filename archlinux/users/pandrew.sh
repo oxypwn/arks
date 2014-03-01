65fb6ceb58dd6177361c3ebf6170f88b3062ecbd
pandrew <letters@paulnotcom.se>
2014-03-01 10:03:13 +0100

    add users to group to  allow ssh in

--- archlinux/users/pandrew.sh
+++ archlinux/users/pandrew.sh
@@ -3,6 +3,6 @@
 # load userspecific settings from here
 USERNAME=pandrew
 GROUP="users"
-ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,docker,sys,wireshark,vboxusers,kvm,libvirt"
+ADDTOGROUPS="permit_ssh_in,audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,docker,sys,wireshark,vboxusers,kvm,libvirt"
 LOGIN_SHELL=/bin/zsh
 EMAIL=letters@paulnotcom.se
--- archlinux/users/vagrant.sh
+++ archlinux/users/vagrant.sh
@@ -2,7 +2,7 @@
 # load userspecific settings from here
 USERNAME=vagrant
 GROUP="users"
-ADDTOGROUPS="audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,sys,wireshark,vboxusers,kvm,libvirt"
+ADDTOGROUPS="permit_ssh_in,audio,lp,optical,storage,video,games,power,scanner,network,wheel,sudo,sys,wireshark,vboxusers,kvm,libvirt"
 # NOTICE! Using a shell not listed in /etc/shells with full path will dissallow login. Be shure to have correct path to shell.
 LOGIN_SHELL=/bin/bash
 EMAIL=vagrant.insecure.key@gmail.com
